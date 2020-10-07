815ef2cb1e04a5251839341f61ea5bfd8f168f78
Tyrell <tlframe2@gmail.com>
2020-10-07 16:05:10 -0400

    testing

--- app.py
+++ app.py
@@ -12,11 +12,12 @@ mongo = PyMongo(app)
 def pokedex():
     submitted_name = request.values.get('Body', '').lower()
     pokemon_result = mongo.db.pokemon.find_one_or_404({"name": name})
-    body = f"{pokemon_result['name']}\n{pokemon_result['description']}"
+    #body = f"{pokemon_result['name']}\n{pokemon_result['description']}"
     resp = MessagingResponse()
     msg = resp.message()
-    msg.body(body)
-    msg.media(pokemon_result['image_url'])
+    #msg.body(body)
+    msg.body(f"body is {submitted_name}")
+    #msg.media(pokemon_result['image_url'])
 
     return str(resp)
 
