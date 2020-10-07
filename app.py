7e94c637b1a279bf77af97e85acc8b738cdd668e
Tyrell <tlframe2@gmail.com>
2020-10-07 16:06:02 -0400

    fixed variable name

--- app.py
+++ app.py
@@ -11,13 +11,12 @@ mongo = PyMongo(app)
 @app.route('/pokedex', methods=['POST'])
 def pokedex():
     submitted_name = request.values.get('Body', '').lower()
-    pokemon_result = mongo.db.pokemon.find_one_or_404({"name": name})
-    #body = f"{pokemon_result['name']}\n{pokemon_result['description']}"
+    pokemon_result = mongo.db.pokemon.find_one_or_404({"name": submitted_name})
+    body = f"{pokemon_result['name']}\n{pokemon_result['description']}"
     resp = MessagingResponse()
     msg = resp.message()
-    #msg.body(body)
-    msg.body(f"body is {submitted_name}")
-    #msg.media(pokemon_result['image_url'])
+    msg.body(body)
+    msg.media(pokemon_result['image_url'])
 
     return str(resp)
 
